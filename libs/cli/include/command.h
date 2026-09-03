d3eebcf7d5122be0529b822a01dc2e2bb82342a1
igotsomesalt <burkelowrey67@gmail.com>
2026-09-02 20:41:42 -0600

    refactor: move command related files

--- libs/cli/include/command.h
+++ libs/cli/include/command.h
@@ -8,42 +8,7 @@
 
 namespace mako {
 
-    // ______________________________________________________________________
-    // An executable command interface that defines an execution contract
-    // as well as metadata contracts.
-    class Command {
-    private:
-        std::string name;
-        std::string description;
-        std::string usage;
-
-    public:
-
-        Command(const std::string& _name, const std::string& _description, const std::string& _usage) :
-            name(std::move(_name)), 
-            description(std::move(_description)), 
-            usage(std::move(_usage)) {}
-        virtual ~Command() = default;
-
-        // Command metadata
-        inline const std::string& name() const {
-            return name;
-        };
-
-        inline const std::string& description() const {
-            return description;
-        };
-     
-        inline const std::string& usage() const {
-            return usage;
-        }
-
-        // Execute the command.
-        virtual bool execute(const CommandExecutionContext&) = 0;
-
-    };
-
-    // Execution context.
+     // Execution context.
     class CommandExecutionContext {
     private:
         runtime::Runtime& runtime;
@@ -82,6 +47,41 @@ namespace mako {
         inline const bool interactive() const {
             return interactive;
         }
+    };
+
+    // ______________________________________________________________________
+    // An executable command interface that defines an execution contract
+    // as well as metadata contracts.
+    class Command {
+    private:
+        const std::string _name;
+        const std::string _description;
+        const std::string _usage;
+
+    public:
+
+        Command(const std::string& name, const std::string& description, const std::string& usage) :
+            _name(std::move(name)), 
+            _description(std::move(description)), 
+            _usage(std::move(usage)) {}
+        virtual ~Command() = default;
+
+        // Command metadata
+        inline const std::string& name() const {
+            return _name;
+        };
+
+        inline const std::string& description() const {
+            return _description;
+        };
+     
+        inline const std::string& usage() const {
+            return _usage;
+        };
+
+        // Execute the command.
+        virtual bool execute(const CommandExecutionContext&) = 0  ;
+
     };
 
     // Adds a node to the graph.
