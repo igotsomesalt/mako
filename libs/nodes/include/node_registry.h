a8e33d78cfe6aa9646a5de4aca972b0253e5f739
igotsomesalt <burkelowrey67@gmail.com>
2026-09-06 00:31:14 -0600

    feat: node registry

---
+++ libs/nodes/include/node_registry.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <unordered_map>
+#include <memory>
+#include <functional>
+#include <node.h>
+
+namespace mako::node {
+
+    class NodeRegistry {
+
+    private:
+        using Factory = std::function<std::unique_ptr<Node>()>;
+        std::unordered_map<std::string, Factory> nodes;
+
+    public:
+        template<typename T>
+        inline void register_node(const std::string& key) {
+            nodes[key] = [] {
+                return std::make_unique<T>();
+            };
+        }
+
+        inline std::unique_ptr<Node> create(const std::string& key) const {
+            auto it = nodes.find(key);
+            if (it == nodes.end()) return nullptr;
+            return it->second();
+        }
+    };
+}
