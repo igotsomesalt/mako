57df5b063e36b5e4b194dd4fc4ba008cd62dbefb
igotsomesalt <burkelowrey67@gmail.com>
2026-09-07 16:29:12 -0600

    feat: add token_type_name helper function

--- libs/cli/include/token.h
+++ libs/cli/include/token.h
@@ -11,6 +11,19 @@ namespace mako {
         ENDLINE, UNKOWN
     };
 
+    inline std::string_view token_type_name(TokenType type) {
+        switch (type) {
+            case TokenType::IDENTIFIER: return "identifier";
+            case TokenType::NUMBER:     return "number";
+            case TokenType::STRING:     return "string";
+            case TokenType::DASH:       return "dash";
+            case TokenType::DOUBLE_DASH:return "double dash";
+            case TokenType::ENDLINE:    return "end line";
+        }
+
+    return "UNKNOWN";
+}
+
     struct Token {
         TokenType type = TokenType::UNKOWN;
         std::string lexeme = "";
