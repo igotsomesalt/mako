42fc05ca227535b3e85e5c18474fd71d259822dd
igotsomesalt <burkelowrey67@gmail.com>
2026-09-07 16:27:15 -0600

    feat: add node execution

--- libs/cli/src/Command.cpp
+++ libs/cli/src/Command.cpp
@@ -1,8 +1,28 @@
+#include <format>
 #include <command.h>
 
 namespace mako {
 
-    int AddCommand::execute(const CommandExecutionContext& ctx) {
-        return true;
+    int AddCommand::execute(CommandExecutionContext& ctx) {
+        if (args.size() == 0) {
+            ctx.err() << "Missing argument" << std::endl;
+            return 0;
+        }
+
+        if (TokenType t = args[0].type; t != TokenType::IDENTIFIER) {
+            ctx.err() << std::format("Expected identifier but got {}", token_type_name(t)) << std::endl;
+            return 0;
+        }
+
+        runtime::Result result = ctx.runtime().add_node(args[0].lexeme);
+        
+        if (!result.success) {
+            ctx.err() << result.err_msg << std::endl;
+            return 0;
+        }
+
+        ctx.out() << result.msg << std::endl;
+
+        return 1;
     }
 }
